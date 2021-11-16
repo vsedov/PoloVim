3f9d94bc04e6396f4dd811c0c7bc2e6a2905f49f
vsedov <viv.sedov@hotmail.com>
2021-11-16 02:49:00 +0000

    added some stuff back

--- init.lua
+++ init.lua
@@ -36,19 +36,6 @@ augroup END
 	false
 )
 
--- Using blugins using hlsearch and some other plugins on teh side .
-vim.api.nvim_exec(
-	[[
-augroup ClearSearchHL
-  autocmd!
-  autocmd CmdlineEnter /,\? set hlsearch
-  autocmd CmdlineLeave /,\? set nohlsearch
-  autocmd CmdlineLeave /,\? lua require('highlight_current_n')['/,?']()
-augroup END
-]],
-	false
-)
-
 vim.api.nvim_exec(
 	[[
 augroup matchup_matchparen_highlight
@@ -87,21 +74,21 @@ vim.fn.sign_define("DiagnosticSignWarning", { text = "", texthl = "LspDiagnos
 vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "LspDiagnosticsSignInformation" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "LspDiagnosticsSignHint" })
 
-cmd [[
+vim.api.nvim_exec([[
     augroup VimStartup
         autocmd BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit' | exe "normal! g`\"" | endif
     augroup END
-]] -- restore cursor position (" -- mark of last cursor position)
+]], false) -- restore cursor position (" -- mark of last cursor position)
 
 
 
-vim.cmd [[
+vim.api.nvim_exec([[
 augroup AutoSaveFolds | autocmd!
   autocmd BufWinLeave,BufLeave,BufWritePost ?* nested silent! mkview!
   autocmd BufWinEnter ?* silent! loadview
 augroup END
 
-]]
+]], false )
 
 
 vim.api.nvim_exec([[
@@ -110,6 +97,77 @@ augroup pythondebug
   autocmd FileType python map <buffer> <Leader><C-S> :update<CR>:exec '!python' shellescape(@%, 1)<CR>
   autocmd FileType python map <buffer> <Leader>dk :update<CR>:sp term://python -m pdb %<CR>
 augroup END
+]], false)
+
+
+
+
+
+
+vim.api.nvim_exec(
+  [[
+" Setting FileType:
+augroup ensureFileType
+  " Make sure all markdown files have the correct filetype set
+  au BufRead,BufNewFile *.{md,md.erb,markdown,mdown,mkd,mkdn,txt} set filetype=markdown
+  au BufNewFile,BufRead .flake8 set filetype=ini
+  au BufNewFile,BufRead cronfile set filetype=sh
+  au BufNewFile,BufRead *.{sh,txt,env*,flaskenv} set filetype=sh
+  au BufNewFile,BufRead *aliases set filetype=zsh
+  au BufNewFile,BufRead *.nix set filetype=nix
+  au BufNewFile,BufRead .gitconfig set filetype=conf
+  au BufNewFile,BufRead *.conf set filetype=config
+  au BufNewFile,BufRead *.{kubeconfig,yml,yaml} set filetype=yaml syntax=on
+augroup end
+" Generic:
+augroup generic
+  au Filetype gitcommit setlocal spell textwidth=72
+  au FileType git setlocal foldlevel=20  " open all unfolded
+  au Filetype vim setlocal tabstop=2 foldmethod=marker
+  au FileType *.ya?ml setlocal shiftwidth=2 tabstop=2 softtabstop=2
+  au FileType sh,zsh setlocal foldmethod=marker foldlevel=10
+  au FileType markdown setlocal wrap wrapmargin=2 textwidth=140 spell
+  au FileType lua setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()
+augroup end
+" EdgeDB
+augroup edgeql
+  au BufNewFile,BufRead *.edgeql setf edgeql
+  au BufNewFile,BufRead *.esdl setf edgeql
+  au FileType edgeql setlocal commentstring=#%s
+augroup end
+
+" Frontend:
+augroup frontend
+  " HTML
+  autocmd FileType html setlocal shiftwidth=2 tabstop=2 softtabstop=2 expandtab
+  autocmd FileType html setlocal foldmethod=syntax nowrap foldlevel=4
+  " JSON
+  autocmd FileType json setlocal foldmethod=syntax foldlevel=20
+  " JS / TS / Vue
+  autocmd FileType vue,typescript setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()
+augroup end
+]],
+  false
+)
+
+
+vim.api.nvim_exec([[
+
+autocmd CmdlineLeave /,\? lua require('highlight_current_n')['/,?']()
+nmap * *N
+
+" Some QOL autocommands
+augroup ClearSearchHL
+  autocmd!
+  " You may only want to see hlsearch /while/ searching, you can automatically
+  " toggle hlsearch with the following autocommands
+  autocmd CmdlineEnter /,\? set hlsearch
+  autocmd CmdlineLeave /,\? set nohlsearch
+  " this will apply similar n|N highlighting to the first search result
+  " careful with escaping ? in lua, you may need \\?
+  autocmd CmdlineLeave /,\? lua require('highlight_current_n')['/,?']()
+augroup END
+
 ]], false)
 
 
