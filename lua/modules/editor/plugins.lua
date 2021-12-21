bc16b1bfcd0c7cb3991dcebc4d09687792db643b
vsedov <viv.sedov@hotmail.com>
2021-12-21 19:06:45 +0000

    Fixed telescope issue with norg

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -263,9 +263,8 @@ editor["nvim-neorg/neorg"]={
     config = function()
   require("packer").loader("plenary.nvim")
   require("packer").loader("nvim-treesitter")
-  require("packer").loader("neorg-telescope")
   require("packer").loader("telescope.nvim")
-
+ 
 
   require('neorg').setup {
     -- Tell Neorg what modules to load
@@ -282,29 +281,13 @@ editor["nvim-neorg/neorg"]={
         }
       },
       ["core.integrations.telescope"] = {} -- Enable the telescope module
-    }
+    
   }
 
 
-    local neorg_callbacks = require("neorg.callbacks")
-
-  neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-    -- Map all the below keybinds only when the "norg" mode is active
-    keybinds.map_event_to_mode("norg", {
-      n = { -- Bind keys in normal mode
-        {"<C-s>", "core.integrations.telescope.find_linkable"}
-      },
-
-      i = { -- Bind in insert mode
-        {"<C-k>", "core.integrations.telescope.insert_link"}
-      }
-    }, {silent = true, noremap = true})
-  end)
-
-    end,
-
-
   }
+end
+}
 
 
 editor["psf/black"] = { ft = "python" }
