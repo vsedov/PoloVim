c3c169806069c6da115b72a43f1b2feb0b9de57d
vsedov <viv.sedov@hotmail.com>
2021-12-23 19:40:37 +0000

    Goto PReview lazy loaded within lazy.lua for only lsp

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -85,7 +85,7 @@ function Lazyload()
   if load_lsp then
     loader("nvim-lspconfig") -- null-ls.nvim
     loader("lsp_signature.nvim")
-
+    loader("goto-preview")
     -- For the timem  null-ls does not work well with jdtls
     if vim.bo.filetype ~= "java" then
       loader("null-ls.nvim")
