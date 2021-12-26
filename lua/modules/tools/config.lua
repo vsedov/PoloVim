90be2cda1aa8cb671c0bf50564a94b30445cca07
vsedov <viv.sedov@hotmail.com>
2021-12-26 17:05:59 +0000

    updated auto sessions config

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -18,12 +18,12 @@ end
 function config.session()
   local opts = {
     log_level = "info",
+    auto_session_suppress_dirs = { "~/", "~/Projects" },
     auto_session_enable_last_session = false,
     auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
     auto_session_enabled = true,
     auto_save_enabled = nil,
     auto_restore_enabled = nil,
-    auto_session_suppress_dirs = nil,
   }
   require("auto-session").setup(opts)
 end
