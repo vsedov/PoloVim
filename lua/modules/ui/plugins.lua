bd9275a48c771de351d34954b2401e4f4720c015
vsedov <viv.sedov@hotmail.com>
2021-12-26 17:06:10 +0000

    changed setup to evil for startup

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -44,7 +44,7 @@ ui["startup-nvim/startup.nvim"] = {
 
   requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   config = function()
-    require("startup").setup({ theme = "dashboard" })
+    require("startup").setup({ theme = "evil" })
   end,
 }
 
