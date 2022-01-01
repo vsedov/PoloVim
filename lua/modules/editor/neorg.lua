60596093d91fb230dcd46d3d1b002a0adf5e46d9
vsedov <viv.sedov@hotmail.com>
2022-01-01 11:14:09 +0000

    neorgsetup for gtd and different workspace + telescope

--- lua/modules/editor/neorg.lua
+++ lua/modules/editor/neorg.lua
@@ -1,22 +1,111 @@
-require("packer").loader("plenary.nvim")
-require("packer").loader("nvim-treesitter")
-require("packer").loader("telescope.nvim")
-require("packer").loader("neorg-telescope")
+local loader = require("packer").loader
+-- Load all main modules required
+loader("plenary.nvim")
+loader("nvim-treesitter")
+
+loader("telescope.nvim")
+loader("neorg-telescope")
+loader("zen-mode.nvim")
+
+local neorg_callbacks = require("neorg.callbacks")
+-- neorg_callbacks.on_event("core.autocommands.events.bufenter", function(event, event_content)
+-- vim.cmd([[PackerLoad nvim-cmp]])
+neorg_callbacks.on_event("core.started", function(event, event_content)
+  require("neorg").setup({
+    load = {
+      ["core.integrations.telescope"] = {},
+    },
+  })
+  require("telescope").setup({})
+end)
 
 require("neorg").setup({
-  -- Tell Neorg what modules to load
+
   load = {
+    ["core.integrations.telescope"] = {}, -- Enable the telescope module
     ["core.defaults"] = {}, -- Load all the default modules
-    ["core.norg.concealer"] = {}, -- Allows for use of icons
-    ["core.norg.dirman"] = { -- Manage your directories with Neorg
-      config = { workspaces = { my_workspace = "~/neorg" } },
-    },
-    ["core.keybinds"] = { -- Configure core.keybinds
+    ["core.norg.concealer"] = {
       config = {
-        default_keybinds = true, -- Generate the default keybinds
-        neorg_leader = "<Leader>o", -- This is the default if unspecified
+        icon_preset = "diamond",
+        icons = {
+          marker = {
+            icon = " ",
+          },
+          todo = {
+            enable = true,
+            pending = {
+              -- icon = ""
+              icon = "",
+            },
+            uncertain = {
+              icon = "?",
+            },
+            urgent = {
+              icon = "",
+            },
+            on_hold = {
+              icon = "",
+            },
+            cancelled = {
+              icon = "",
+            },
+          },
+        },
       },
     },
-    ["core.integrations.telescope"] = {}, -- Enable the telescope module
+    ["core.presenter"] = {
+      config = {
+        zen_mode = "zen-mode",
+      },
+    },
+    ["core.keybinds"] = {
+      config = {
+        default_keybinds = true,
+        neorg_leader = "<Leader>o",
+      },
+    },
+    ["core.norg.dirman"] = {
+      config = {
+        workspaces = {
+          my_workspace = "~/neorg",
+          example_ws = "~/example_workspaces/gtd/",
+          gtd = "~/gtd",
+          notes = "~notes",
+        },
+      },
+    },
+    ["core.gtd.base"] = {
+      config = {
+        workspace = "gtd",
+      },
+    },
+    -- ["core.norg.qol.toc"] = {
+    --   config = {
+    --     close_split_on_jump = false,
+    --     toc_split_placement = "left",
+    --   },
+    -- },
+    ["core.norg.journal"] = {
+      config = {
+        journal_folder = "my_journal",
+        use_folders = false,
+      },
+    },
   },
 })
+
+local neorg_callbacks = require("neorg.callbacks")
+
+neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+  -- Map all the below keybinds only when the "norg" mode is active
+  keybinds.map_event_to_mode("norg", {
+    n = { -- Bind keys in normal mode
+      { "<c-s>", "core.integrations.telescope.find_linkable" },
+      { "<c-i>", "core.integrations.telescope.search_headings" },
+      { "<c-k>", "core.integrations.telescope.insert_link" },
+    },
+    i = { -- Bind in insert mode
+      { "<c-k>", "core.integrations.telescope.insert_link" },
+    },
+  }, { silent = true, noremap = true })
+end)
