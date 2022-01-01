15b6f8cb3a09636288c668af4b351f1298c4f9f2
vsedov <viv.sedov@hotmail.com>
2022-01-01 11:11:33 +0000

    changed notify loading - with popup and moved around loading for plugins that are called in Lazy

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -59,6 +59,38 @@ function config.nvim_bufferline()
   })
 end
 
+function config.notify()
+  require("notify").setup({
+    -- Animation style (see below for details)
+    stages = "fade_in_slide_out",
+
+    -- Function called when a new window is opened, use for changing win settings/config
+    on_open = nil,
+
+    -- Function called when a window is closed
+    on_close = nil,
+
+    -- Render function for notifications. See notify-render()
+    render = "default",
+
+    -- Default timeout for notifications
+    timeout = 5000,
+
+    -- For stages that change opacity this is treated as the highlight behind the window
+    -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
+    background_colour = "Normal",
+
+    -- Minimum width for notification windows
+    minimum_width = 50,
+
+    -- Icons for the different levels
+    icons = {
+      TRACE = "✎",
+    },
+  })
+  require("telescope").load_extension("notify")
+end
+
 -- vim.cmd(
 --   [[
 --   fun! s:disable_statusline(bn)
