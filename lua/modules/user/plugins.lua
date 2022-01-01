6c407c4a657f44e734c90b1caa4fdfdd6a0a41ab
vsedov <viv.sedov@hotmail.com>
2022-01-01 11:18:06 +0000

    Test Plugin

--- lua/modules/user/plugins.lua
+++ lua/modules/user/plugins.lua
@@ -1,5 +1,14 @@
 local user = {}
 local conf = require("modules.user.config")
 
+user["~/GitHub/ytmmusic.lua"]={
+	require = {"rcarriga/nvim-notify", "nvim-lua/plenary.nvim"},
+	config = function()
+		require("ytmmusic")
+ 	end
+}
+
+
+
 -- your plugin config
 return user
