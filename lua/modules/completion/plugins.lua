9c7be228c4bfe4ab8070b78a1bc56e221ffa7517
vsedov <viv.sedov@hotmail.com>
2022-01-03 15:11:55 +0000

    better telescope / plugin for lua start properly using luadev

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -8,7 +8,6 @@ completion["neovim/nvim-lspconfig"] = {
 
   requires = {
     { "nvim-lua/lsp_extensions.nvim", opt = true },
-    { "tjdevries/nlua.nvim", opt = true },
     { "folke/lsp-colors.nvim", opt = true },
     { "williamboman/nvim-lsp-installer", opt = true },
     { "tami5/lspsaga.nvim", opt = true, cmd = "Lspsaga" },
@@ -140,22 +139,20 @@ completion["kristijanhusak/vim-dadbod-completion"] = {
 }
 
 completion["nvim-telescope/telescope.nvim"] = {
-  cmd = "Telescope",
   module = { "telescope", "utils.telescope" },
   config = conf.telescope,
   setup = conf.telescope_preload,
   requires = {
-    { "nvim-lua/popup.nvim" }, -- test
+    { "nvim-lua/popup.nvim", opt = true }, -- test
     { "nvim-neorg/neorg-telescope", opt = true },
     { "nvim-lua/plenary.nvim", opt = true },
     { "nvim-telescope/telescope-fzy-native.nvim", opt = true },
     { "nvim-telescope/telescope-fzf-native.nvim", run = "make", opt = true },
     { "nvim-telescope/telescope-live-grep-raw.nvim", opt = true },
-    { "nvim-telescope/telescope-file-browser.nvim", opt = true}
+    { "nvim-telescope/telescope-file-browser.nvim", opt = true },
   },
   opt = true,
 }
-
 
 completion["mattn/emmet-vim"] = {
   event = "InsertEnter",
