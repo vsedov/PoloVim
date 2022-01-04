6c352cc975cd65c6b569d3e3ba8ff025220092f7
vsedov <viv.sedov@hotmail.com>
2022-01-04 10:29:44 +0000

    yoink

--- lua/modules/editor/which_key.lua
+++ lua/modules/editor/which_key.lua
@@ -1,3 +1,55 @@
 local wk = require("which-key")
 
--- TODO
+
+wk.setup({
+  window = {
+    border = "none", -- none, single, double, shadow
+    margin = { 1, 0, 1, 0 },
+    winblend = 20,
+  },
+})
+
+wk.register({
+
+c = {
+    name = "+Comment",
+    c = "Current line",
+    o = "Enter insert below",
+    O = "Enter insert above",
+    A = "Enter insert eol",
+  },
+  b = {
+    name = "+Block Comment",
+    c = "Current Line",
+  },
+  n = {
+    name = "Goto Next, Incremental Selection, List",
+    n = { "Incremental Selection" },
+    d = { "Goto Definition" },
+    D = { "List Definitions" },
+    O = { "List Definitions TOC" },
+    ["0"] = { "List Lsp Workspace" },
+    f = "Start Outer Function",
+    u = "Next Usage",
+    i = {
+      name = "Inner",
+      c = { "Start Call" },
+      f = { "Start Function" },
+      F = { "End Function" },
+      C = { "End Call" },
+    },
+    F = { "End Outer Function" },
+    p = { "Start Inner Parameter" },
+    c = { "Start Outer Call" },
+    P = { "End Inner Parameter" },
+    C = { "End Outer Call" },
+  },
+
+  ["<leader>"] = {
+    f = {
+      name = "Telescope modules",
+      b = { '<cmd>lua require("utils.telescope").file_browser()<cr>', "file_browser" },
+    },
+  },
+
+})
