8ebd25209c7dfc281d2ad6877239ee19e469ce79
vsedov <viv.sedov@hotmail.com>
2022-01-04 10:30:13 +0000

    removed any none used plugins

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -15,7 +15,7 @@ ui["windwp/windline.nvim"] = {
   opt = true,
 }
 
-ui["lambdalisue/glyph-palette.vim"] = {}
+-- ui["lambdalisue/glyph-palette.vim"] = {}
 
 ui["akinsho/bufferline.nvim"] = {
   config = conf.nvim_bufferline,
@@ -68,7 +68,6 @@ ui["dstein64/nvim-scrollview"] = {
   config = conf.scrollview,
 }
 
-ui["ray-x/aurora"] = { opt = true, config = conf.aurora }
 ui["folke/tokyonight.nvim"] = {
   opt = true,
   setup = conf.tokyonight,
@@ -101,37 +100,11 @@ ui["rebelot/kanagawa.nvim"] = {
   end,
 }
 
-ui["projekt0n/github-nvim-theme"] = {
-  opt = true,
-  config = function()
-    -- vim.cmd [[hi CursorLine guibg=#353644]]
-    local styles = { "dark", "dark_default", "dimmed" }
-    local v = math.random(1, #styles)
-    local st = styles[v]
-    require("github-theme").setup({
-      function_style = "bold",
-      theme_style = st,
-      sidebars = { "qf", "vista_kind", "terminal", "packer" },
-      colors = { bg_statusline = "#332344" },
-    })
-    -- vim.cmd([[highlight StatusLine guibg='#A3B3C4']])
-    vim.cmd([[highlight ColorColumn guibg='#335364']])
-    vim.cmd([[doautocmd ColorScheme]])
-  end,
-}
-
 -- ui["ChristianChiarulli/nvcode-color-schemes.vim"] = {opt = true, config = conf.nvcode}
 
 -- cant config cursor line
 -- ui["rafamadriz/neon"] = {opt = true, config = conf.neon}
 
-ui["ray-x/starry.nvim"] = {
-  opt = true,
-  setup = conf.starry,
-  config = function()
-    require("starry").set()
-  end,
-}
 
 ui["kazhala/close-buffers.nvim"] = {
   cmd = { "BDelete", "BWipeout" },
