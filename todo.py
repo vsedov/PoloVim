00c6709cdd6a606131425bb5c3b216598bf38a7c
vsedov <viv.sedov@hotmail.com>
2022-01-27 19:52:04 +0000

    dummy file

---
+++ todo.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Vivian Sedov
+#
+# File Name: todo.py
+__author__ = "Vivian Sedov"
+__email__ = "viv.sv@hotmail.com"
+
+import pyinspect as pi
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    pi.install_traceback()
+    main()
