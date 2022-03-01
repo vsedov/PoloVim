4fe4c2d766ea239b2f223e48f792cf15ef0bae35
vsedov <viv.sedov@hotmail.com>
2022-03-01 13:06:48 +0000

    norg loading

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -6,3 +6,18 @@ vim.cmd([[source ~/.config/nvim/clipboard_neorg.vim]])
 
 -- Not sure if this needs to be on or not
 -- vim.o.spell = 1
+
+-- wait till norg is fixed
+
+local neorg = require("neorg")
+local function load_completion()
+  neorg.modules.load_module("core.norg.completion", nil, {
+    engine = "nvim-cmp", -- Choose your completion engine here
+  })
+end
+
+if neorg.is_loaded() then
+  load_completion()
+else -- Otherwise wait until Neorg gets started and load the completion module then
+  neorg.callbacks.on_event("core.started", load_completion)
+end
