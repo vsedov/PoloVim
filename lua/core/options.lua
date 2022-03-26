d30d4bd099528f36d37569dd49ef6aa9fa687663
vsedov <viv.sedov@hotmail.com>
2022-03-26 16:58:34 +0000

    test: signcolumn 3 over auto2

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -110,7 +110,7 @@ local function load_options()
         number = true,
         colorcolumn = "80",
         foldenable = true,
-        signcolumn = "auto:1", --auto auto:2  "number"
+        signcolumn = "yes:3", --[auto yes] yes:3  auto:2  "number" auto: 2-4
         conceallevel = 2,
         concealcursor = "niv",
     }
