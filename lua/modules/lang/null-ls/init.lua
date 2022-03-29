e7d17269201d776d01f17d52106591da75911951
vsedov <viv.sedov@hotmail.com>
2022-03-29 17:41:10 +0100

    null-ls revert back to old function call, to avoid conflicts with lsp, this needs to be refactored into ftplugin soon

--- lua/modules/lang/null-ls/init.lua
+++ lua/modules/lang/null-ls/init.lua
@@ -175,21 +175,21 @@ return {
                 "tsconfig.json",
                 ".git"
             ),
-            on_attach = require("modules.completion.lsp.utils").get_common_opts,
-            -- on_attach = function(client)
-            --     -- I dont want any formating on python files.
-            --     if client.resolved_capabilities.document_formatting then
-            --         vim.diagnostic.config({
-            --             virtual_text = false,
-            --         })
-            --         vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting(nil, 800)")
-            --         -- Convert this using vim.api.nvim_create_autocmd()
-            --         -- vim.api.nvim_create_autocmd("BufWritePre", {
-            --         --     pattern = "*",
-            --         --     command = "lua vim.lsp.buf.formatting()",
-            --         -- })
-            --     end
-            -- end,
+            -- on_attach = require("modules.completion.lsp.utils").get_common_opts,
+            on_attach = function(client)
+                -- I dont want any formating on python files.
+                if client.resolved_capabilities.document_formatting then
+                    vim.diagnostic.config({
+                        virtual_text = false,
+                    })
+                    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting(nil, 800)")
+                    -- Convert this using vim.api.nvim_create_autocmd()
+                    -- vim.api.nvim_create_autocmd("BufWritePre", {
+                    --     pattern = "*",
+                    --     command = "lua vim.lsp.buf.formatting()",
+                    -- })
+                end
+            end,
         }
 
         null_ls.setup(cfg)
