8533fca5b72df2b79b2343b103e1406492e43a3e
vsedov <vsedov@users.noreply.github.com>
2022-04-03 01:06:17 +0000

    chore: autoformat with stylua

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ vim.opt.shadafile = "NONE"
 require("core")
 require("overwrite")
 vim.schedule(function()
-  vim.opt.shadafile = "" 
-  vim.cmd([[ silent! rsh ]])
+    vim.opt.shadafile = ""
+    vim.cmd([[ silent! rsh ]])
 end)
