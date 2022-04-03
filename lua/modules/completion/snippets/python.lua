50766cba44a282df42e0122603628fafdd0983fb
vsedov <viv.sedov@hotmail.com>
2022-04-03 09:36:26 +0100

    feat: new snippets

--- lua/modules/completion/snippets/python.lua
+++ lua/modules/completion/snippets/python.lua
@@ -7,72 +7,20 @@ local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
-local r = ls.restore_node
+-- local r = ls.restore_node
 local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
+-- local rep = require("luasnip.extras").rep
 local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
+-- local m = require("luasnip.extras").match
+-- local n = require("luasnip.extras").nonempty
 local dl = require("luasnip.extras").dynamic_lambda
 local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local types = require("luasnip.util.types")
+-- local conds = require("luasnip.extras.expand_conditions")
 
-local function saved_text(args, snip, old_state, placeholder)
-    local nodes = {}
-    if not old_state then
-        old_state = {}
-    end
-    if not placeholder then
-        placeholder = {}
-    end
-    local indent = placeholder.indent and "\t" or ""
-
-    if snip.snippet.env and snip.snippet.env.SELECT_DEDENT and #snip.snippet.env.SELECT_DEDENT > 0 then
-        local lines = vim.deepcopy(snip.snippet.env.SELECT_DEDENT)
-        -- local indent_level = require'utils.buffers'.get_indent_block_level(lines)
-        -- lines = require'utils.buffers'.indent(lines, -indent_level)
-        -- TODO: We may need to use an indent indepente node to avoid indenting empty lines
-        for idx = 1, #lines do
-            local line = indent .. lines[idx]
-            local node = idx == #lines and { line } or { line, "" }
-            table.insert(nodes, t(node))
-        end
-    else
-        local text = placeholder.text or "# placeholder"
-        if indent ~= "" then
-            table.insert(nodes, t(indent))
-        end
-        table.insert(nodes, i(1, text))
-    end
-
-    local snip_node = sn(nil, nodes)
-    snip_node.old_state = old_state
-    return snip_node
-end
-
-local function surround_with_func(args, snip, old_state, placeholder)
-    local nodes = {}
-    if not old_state then
-        old_state = {}
-    end
-    if not placeholder then
-        placeholder = {}
-    end
-
-    if snip.snippet.env and snip.snippet.env.SELECT_RAW and #snip.snippet.env.SELECT_RAW == 1 then
-        local node = snip.snippet.env.SELECT_RAW[1]
-        table.insert(nodes, t(node))
-    else
-        local text = placeholder.text or "placeholder"
-        table.insert(nodes, i(1, text))
-    end
-
-    local snip_node = sn(nil, nodes)
-    snip_node.old_state = old_state
-    return snip_node
-end
+local saved_text = require("modules.completion.snippets.sniputils").saved_text
+local else_clause = require("modules.completion.snippets.sniputils").else_clause
 
 local function python_class_init(args, snip, old_state, placeholder)
     local nodes = {}
@@ -107,21 +55,6 @@ local function python_dataclass(args, snip, old_state, placeholder)
     local nodes = {}
 
     table.insert(nodes, snip.captures[1] == "d" and t({ "@dataclass", "" }) or t({ "" }))
-
-    local snip_node = sn(nil, nodes)
-    snip_node.old_state = old_state
-    return snip_node
-end
-
-local function else_clause(args, snip, old_state, placeholder)
-    local nodes = {}
-
-    if snip.captures[1] == "e" then
-        table.insert(nodes, t({ "", "else", "\t" }))
-        table.insert(nodes, i(1, "pass"))
-    else
-        table.insert(nodes, t({ "" }))
-    end
 
     local snip_node = sn(nil, nodes)
     snip_node.old_state = old_state
@@ -172,6 +105,32 @@ local function pycdoc(args, ostate)
     return snip
 end
 
+local auto_snippets = {
+
+    s(
+        { trig = "fro(m?)", regTrig = true },
+        fmt([[from {} import {}]], {
+            i(1, "sys"),
+            i(2, "path"),
+        })
+    ),
+    s(
+        { trig = "if(e?)", regTrig = true },
+        fmt(
+            [[
+    if {}:
+    {}{}
+    ]],
+            {
+                i(1, "condition"),
+                d(2, saved_text, {}, { user_args = { { text = "pass", indent = true } } }),
+                d(3, else_clause, {}, {}),
+            }
+        )
+    ),
+}
+ls.add_snippets("python", auto_snippets, { type = "autosnippets" })
+
 local M = {
     s({ trig = "cls", dscr = "Documented Class Structure" }, {
         t("class "),
@@ -207,47 +166,67 @@ local M = {
         d(3, pyfdoc, { 2 }, { 1 }),
     }),
 
-    s("for", {
-        t({ "for " }),
-        i(1, "i"),
-        t({ " in " }),
-        i(2, "Iterator"),
-        t({ ":", "" }),
-        d(3, saved_text, {}, { text = "pass", indent = true }),
-    }),
-    s("ran", {
-        t({ "range(" }),
-        i(1, "0"),
-        t({ ", " }),
-        i(2, "limit"),
-        t({ ")" }),
-    }),
-    s("imp", {
-        t({ "import " }),
-        i(1, "sys"),
-    }),
+    s(
+        "for",
+        fmt(
+            [[
+    for {} in {}:
+    {}
+    ]],
+            {
+                i(1, "i"),
+                i(2, "iterator"),
+                d(3, saved_text, {}, { user_args = { { text = "pass", indent = true } } }),
+            }
+        )
+    ),
+    s(
+        "pr",
+        fmt([[print({})]], {
+            i(1, "msg"),
+        })
+    ),
+    s(
+        "ran",
+        fmt([[range({}, {})]], {
+            i(1, "0"),
+            i(2, "10"),
+        })
+    ),
+    s(
+        "imp",
+        fmt([[import {}]], {
+            i(1, "sys"),
+        })
+    ),
+    s(
+        { trig = "def", dscr = "Advanced Def" },
+        fmt(
+            [[
+    def {}({}{}):
+    {}
+    ]],
+            {
+                i(1, "name"),
+                p(function()
+            -- stylua: ignore
+            local get_current_class = require('utils.treesitter_utils').get_current_class
+            -- stylua: ignore
+            local has_ts = require('utils.treesitter_utils').has_ts()
+            -- stylua: ignore
+            if has_ts and get_current_class() then
+                -- stylua: ignore
+                return 'self, '
+            end
+            -- stylua: ignore
+            return ''
+                end),
+                i(2, "args"),
+                d(3, saved_text, {}, { user_args = { { text = "pass", indent = true } } }),
+            }
+        )
+    ),
 
-    s({ trig = "from", regTrig = true }, {
-        t({ "from " }),
-        i(1, "os"),
-        t({ " import " }),
-        i(2, "path"),
-    }),
-    s({ trig = "if", regTrig = true }, {
-        t({ "if " }),
-        i(1, "condition"),
-        t({ ":", "" }),
-        d(2, saved_text, {}, { text = "pass", indent = true }),
-        d(3, else_clause, {}, {}),
-    }),
-    s("def", {
-        t({ "def " }),
-        i(1, "name"),
-        t({ "(" }),
-        i(2, "args"),
-        t({ "):", "" }),
-        d(3, saved_text, {}, { text = "pass", indent = true }),
-    }),
     s("try", {
         t({ "try:", "" }),
         d(1, saved_text, {}, { text = "pass", indent = true }),
@@ -318,6 +297,23 @@ local M = {
         t({ '"""', "" }),
         d(5, python_class_init, {}, {}),
     }),
+    -- snippet that calls pi.ask("question")
+    s(
+        { trig = "ask" },
+        fmt([[   pi.ask("{}")]], {
+            i(1, "How do i invert a binary tree?"),
+        })
+    ),
+    -- # Find the functions you're looking for
+    -- pi.search(pi, name='what')
+    -- snippet for this
+    s(
+        { trig = "search" },
+        fmt([[pi.search({}, name='{}')]], {
+            i(1, "what"),
+            i(2, "name"),
+        })
+    ),
 }
 
 return M
--- lua/modules/completion/snippets/sniputils.lua
+++ lua/modules/completion/snippets/sniputils.lua
@@ -354,4 +354,135 @@ utils.pipe = function(fns)
     end
 end
 
+-- TODO: Update this with TS support
+function utils.get_comment(text)
+    vim.validate({
+        text = {
+            text,
+            function(x)
+                return not x or type(x) == type("") or vim.tbl_islist(x)
+            end,
+            "text must be either a string or an array of lines",
+        },
+    })
+    local comment = vim.opt_local.commentstring:get()
+    if not comment:match("%s%%s") then
+        comment = comment:format(" %s")
+    end
+    local comment_str
+    if text then
+        if vim.tbl_islist(text) then
+            comment_str = {}
+            for _, line in ipairs(text) do
+                table.insert(comment_str, comment:format(line))
+            end
+            comment_str = table.concat(comment_str, "\n")
+        else
+            comment_str = comment:format(text)
+        end
+    end
+    return comment_str or comment
+end
+
+function utils.saved_text(args, snip, old_state, user_args)
+    local nodes = {}
+    old_state = old_state or {}
+    user_args = user_args or {}
+
+    local indent = user_args.indent and "\t" or ""
+
+    if snip.snippet.env and snip.snippet.env.SELECT_DEDENT and #snip.snippet.env.SELECT_DEDENT > 0 then
+        local lines = vim.deepcopy(snip.snippet.env.SELECT_DEDENT)
+        -- local indent_level = require'utils.buffers'.get_indent_block_level(lines)
+        -- lines = require'utils.buffers'.indent(lines, -indent_level)
+        -- TODO: We may need to use an indent indepente node to avoid indenting empty lines
+        for idx = 1, #lines do
+            local line = indent .. lines[idx]
+            local node = idx == #lines and { line } or { line, "" }
+            table.insert(nodes, t(node))
+        end
+    else
+        local text = user_args.text or utils.get_comment("code")
+        if indent ~= "" then
+            table.insert(nodes, t(indent))
+        end
+        table.insert(nodes, i(1, text))
+    end
+
+    local snip_node = sn(nil, nodes)
+    snip_node.old_state = old_state
+    return snip_node
+end
+
+function utils.surround_with_func(args, snip, old_state, user_args)
+    local nodes = {}
+    old_state = old_state or {}
+    user_args = user_args or {}
+
+    if snip.snippet.env and snip.snippet.env.SELECT_RAW and #snip.snippet.env.SELECT_RAW == 1 then
+        local node = snip.snippet.env.SELECT_RAW[1]
+        table.insert(nodes, t(node))
+    else
+        local text = user_args.text or "placeholder"
+        table.insert(nodes, i(1, text))
+    end
+
+    local snip_node = sn(nil, nodes)
+    snip_node.old_state = old_state
+    return snip_node
+end
+
+function utils.copy(args)
+    return args[1]
+end
+
+function utils.return_value()
+    local clike = {
+        c = true,
+        cpp = true,
+        java = true,
+    }
+
+    local snippet = { t({ "return " }), i(1, "value") }
+
+    if clike[vim.bo.filetype] then
+        table.insert(snippet, t({ ";" }))
+    end
+
+    return snippet
+end
+
+function utils.else_clause(args, snip, old_state, placeholder)
+    local nodes = {}
+    local ft = vim.opt_local.filetype:get()
+
+    if snip.captures[1] == "e" then
+        if ft == "lua" then
+            table.insert(nodes, t({ "", "else", "\t" }))
+            table.insert(nodes, i(1, utils.get_comment("code")))
+        elseif ft == "python" then
+            table.insert(nodes, t({ "", "else", "\t" }))
+            table.insert(nodes, i(1, "pass"))
+        elseif ft == "sh" or ft == "bash" or ft == "zsh" then
+            table.insert(nodes, t({ "else", "\t" }))
+            table.insert(nodes, i(1, ":"))
+            table.insert(nodes, t({ "", "" }))
+        elseif ft == "go" or ft == "rust" then
+            table.insert(nodes, t({ " else {", "\t" }))
+            table.insert(nodes, i(1, utils.get_comment("code")))
+            table.insert(nodes, t({ "", "}" }))
+        else
+            table.insert(nodes, t({ "", "else {", "\t" }))
+            table.insert(nodes, i(1, utils.get_comment("code")))
+            table.insert(nodes, t({ "", "}" }))
+        end
+    else
+        table.insert(nodes, t({ "" }))
+    end
+
+    local snip_node = sn(nil, nodes)
+    snip_node.old_state = old_state
+    return snip_node
+end
+
 return utils
