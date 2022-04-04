9f8c92c943e91bbf9518fdf531b0f9a7f03ce9db
vsedov <vsedov@users.noreply.github.com>
2022-04-04 00:39:50 +0000

    chore: autoformat with stylua

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -7,8 +7,7 @@ local options = {
     expandtab = true,
     smarttab = true,
     -- colorcolumn = 90, -- Delimit text blocks to N columns
-    conceallevel=2,
-
+    conceallevel = 2,
 }
 
 for k, v in pairs(options) do
@@ -30,37 +29,37 @@ inoreabbrev <buffer> iff if :<left>
 local ts_utils = require("nvim-treesitter.ts_utils")
 
 local toggle_fstring = function()
-   local winnr = 0
-   local cursor = vim.api.nvim_win_get_cursor(winnr)
-   local node = ts_utils.get_node_at_cursor()
+    local winnr = 0
+    local cursor = vim.api.nvim_win_get_cursor(winnr)
+    local node = ts_utils.get_node_at_cursor()
 
-   while (node ~= nil) and (node:type() ~= "string") do
-      node = node:parent()
-   end
-   if node == nil then
-      log:info("f-string: not in string node :(")
-      return
-   end
+    while (node ~= nil) and (node:type() ~= "string") do
+        node = node:parent()
+    end
+    if node == nil then
+        log:info("f-string: not in string node :(")
+        return
+    end
 
-   local srow, scol, ecol, erow = ts_utils.get_vim_range({ node:range() })
-   vim.fn.setcursorcharpos(srow, scol)
-   local char = vim.api.nvim_get_current_line():sub(scol, scol)
-   local is_fstring = (char == "f")
+    local srow, scol, ecol, erow = ts_utils.get_vim_range({ node:range() })
+    vim.fn.setcursorcharpos(srow, scol)
+    local char = vim.api.nvim_get_current_line():sub(scol, scol)
+    local is_fstring = (char == "f")
 
-   if is_fstring then
-      vim.cmd("normal x")
-      -- if cursor is in the same line as text change
-      if srow == cursor[1] then
-         cursor[2] = cursor[2] - 1 -- negative offset to cursor
-      end
-   else
-      vim.cmd("normal if")
-      -- if cursor is in the same line as text change
-      if srow == cursor[1] then
-         cursor[2] = cursor[2] + 1 -- positive offset to cursor
-      end
-   end
-   vim.api.nvim_win_set_cursor(winnr, cursor)
+    if is_fstring then
+        vim.cmd("normal x")
+        -- if cursor is in the same line as text change
+        if srow == cursor[1] then
+            cursor[2] = cursor[2] - 1 -- negative offset to cursor
+        end
+    else
+        vim.cmd("normal if")
+        -- if cursor is in the same line as text change
+        if srow == cursor[1] then
+            cursor[2] = cursor[2] + 1 -- positive offset to cursor
+        end
+    end
+    vim.api.nvim_win_set_cursor(winnr, cursor)
 end
 
-vim.keymap.set('n', 'F', toggle_fstring, { noremap = true })
+vim.keymap.set("n", "F", toggle_fstring, { noremap = true })
