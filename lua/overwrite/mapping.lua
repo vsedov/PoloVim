340547fca84bfc90e63e51071bf8796da3952d41
vsedov <viv.sedov@hotmail.com>
2022-04-18 12:44:38 +0100

    better way to call command on overwrite

--- lua/overwrite/mapping.lua
+++ lua/overwrite/mapping.lua
@@ -3,6 +3,7 @@ local map_cr = bind.map_cr
 local map_cu = bind.map_cu
 local map_cmd = bind.map_cmd
 -- local map_args = bind.map_args
+local add_cmd = vim.api.nvim_create_user_command
 
 local loader = require("packer").loader
 local K = {}
@@ -165,27 +166,27 @@ _G.run_or_test = function(debug)
     end
 end
 
-vim.api.nvim_create_user_command("AbbrivGodMode", function()
+add_cmd("AbbrivGodMode", function()
     vim.cmd([[packadd vim-abbrev]])
 end, { force = true })
 
-vim.api.nvim_create_user_command("DuckStart", function()
+add_cmd("DuckStart", function()
     require("modules.useless.config").launch_duck()
 end, { force = true })
 
-vim.api.nvim_create_user_command("TestStart", function()
+add_cmd("TestStart", function()
     require("modules.lang.language_utils").testStart()
 end, { force = true })
 
-vim.api.nvim_create_user_command("DebugOpen", function()
+add_cmd("DebugOpen", function()
     require("modules.lang.dap").prepare()
 end, { force = true })
 
-vim.api.nvim_create_user_command("HpoonClear", function()
+add_cmd("HpoonClear", function()
     require("harpoon.mark").clear_all()
 end, { force = true })
 
-vim.api.nvim_create_user_command("Hashbang", function()
+add_cmd("Hashbang", function()
     local shells = {
         sh = { "#! /usr/bin/env bash" },
         py = { "#! /usr/bin/env python3" },
@@ -213,29 +214,29 @@ vim.api.nvim_create_user_command("Hashbang", function()
     end
 end, { force = true })
 
-vim.api.nvim_create_user_command("Tags", function()
+add_cmd("Tags", function()
     vim.cmd(
         [[!ctags -R -I EXTERN -I INIT --exclude='build*' --exclude='.vim-src/**' --exclude='node_modules/**' --exclude='venv/**' --exclude='**/site-packages/**' --exclude='data/**' --exclude='dist/**' --exclude='notebooks/**' --exclude='Notebooks/**' --exclude='*graphhopper_data/*.json' --exclude='*graphhopper/*.json' --exclude='*.json' --exclude='qgis/**' *]]
     )
 end, { force = true })
 
-vim.api.nvim_create_user_command("Keymaps", function()
+add_cmd("Keymaps", function()
     require("overwrite.mapping").get_keymaps()
 end, { force = true })
 
-vim.api.nvim_create_user_command("ColourScheme", function()
+add_cmd("ColourScheme", function()
     require("utils.telescope").colorscheme()
 end, { force = true })
 
-vim.api.nvim_create_user_command("BL", function()
+add_cmd("BL", function()
     require("utils.selfunc").blameVirtualText()
 end, { force = true })
 
-vim.api.nvim_create_user_command("BLR", function()
+add_cmd("BLR", function()
     require("utils.selfunc").clearBlameVirtualText()
 end, { force = true })
 
-vim.api.nvim_create_user_command("Diagnostics", function()
+add_cmd("Diagnostics", function()
     vim.cmd("silent lmake! %")
     if #vim.fn.getloclist(0) == 0 then
         vim.cmd("lopen")
@@ -245,12 +246,12 @@ vim.api.nvim_create_user_command("Diagnostics", function()
 end, {
     force = true,
 })
-vim.api.nvim_create_user_command("Format", "silent normal! mxgggqG`x<CR>", {
+add_cmd("Format", "silent normal! mxgggqG`x<CR>", {
     force = true,
 })
 
 -- Adjust Spacing:
-vim.api.nvim_create_user_command("Spaces", function(args)
+add_cmd("Spaces", function(args)
     local wv = vim.fn.winsaveview()
     vim.opt_local.expandtab = true
     vim.cmd("silent execute '%!expand -it" .. args.args .. "'")
@@ -260,7 +261,7 @@ end, {
     force = true,
     nargs = 1,
 })
-vim.api.nvim_create_user_command("Tabs", function(args)
+add_cmd("Tabs", function(args)
     local wv = vim.fn.winsaveview()
     vim.opt_local.expandtab = false
     vim.cmd("silent execute '%!unexpand -t" .. args.args .. "'")
@@ -271,20 +272,50 @@ end, {
     nargs = 1,
 })
 
-vim.api.nvim_create_user_command("NvimEditInit", function(args)
+add_cmd("NvimEditInit", function(args)
     vim.cmd("split | edit $MYVIMRC")
 end, {
     force = true,
     nargs = "*",
 })
-vim.api.nvim_create_user_command("NvimSourceInit", function(args)
+add_cmd("NvimSourceInit", function(args)
     vim.cmd("luafile $MYVIMRC")
 end, {
     force = true,
     nargs = "*",
 })
-vim.api.nvim_create_user_command("TodoLocal", "botright silent! lvimgrep /\\v\\CTODO|FIXME|HACK|PERF/", {})
-vim.api.nvim_create_user_command("Todo", "botright silent! vimgrep /\\v\\CTODO|FIXME|HACK|PERF/ *<CR>", {})
+add_cmd("TodoLocal", "botright silent! lvimgrep /\\v\\CTODO|FIXME|HACK|PERF/", {})
+add_cmd("Todo", "botright silent! vimgrep /\\v\\CTODO|FIXME|HACK|PERF/ *<CR>", {})
+
+add_cmd("CursorNodes", function()
+    local node = require("nvim-treesitter.ts_utils").get_node_at_cursor()
+    while node do
+        dump(node:type())
+        node = node:parent()
+    end
+end, {})
+
+local function ShowLangTree(langtree, indent)
+    local ts = vim.treesitter
+    langtree = langtree or ts.get_parser()
+    indent = indent or ""
+
+    print(indent .. langtree:lang())
+    for _, region in pairs(langtree:included_regions()) do
+        if type(region[1]) == "table" then
+            print(indent .. "  " .. vim.inspect(region))
+        else
+            print(indent .. "  " .. vim.inspect({ region[1]:range() }))
+        end
+    end
+    for lang, child in pairs(langtree._children) do
+        ShowLangTree(child, indent .. "  ")
+    end
+end
+
+add_cmd("LangTree", function()
+    ShowLangTree()
+end, {})
 
 vim.cmd("command! -nargs=+ -complete=file Grep " .. "lua vim.api.nvim_exec([[grep! <args> | redraw! | copen]], true)")
 vim.cmd("command! -nargs=+ -complete=file LGrep " .. "lua vim.api.nvim_exec([[lgrep! <args> | redraw! | lopen]], true)")
