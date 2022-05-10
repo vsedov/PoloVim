a1ec9b93e67ccab922d4673bdf7583534097d89f
vsedov <viv.sedov@hotmail.com>
2022-05-10 23:10:45 +0100

    useless plugins

--- lua/modules/useless/plugins.lua
+++ lua/modules/useless/plugins.lua
@@ -19,5 +19,33 @@ useless["tamton-aquib/duck.nvim"] = {
     config = conf.launch_duck,
 }
 
+useless["tamton-aquib/zone.nvim"] = {
+    cmd = { "Zone" }, -- return {'treadmill', 'dvd', 'vanish'} Zone dvd
+    opt = true,
+    config = function()
+        require("zone").setup({
+            style = "treadmill",
+            after = 30, -- Idle timeout
+            -- More options to come later
+
+            treadmill = {
+                direction = "left",
+                -- Opts for Treadmill style
+            },
+            dvd = {
+                -- Opts for Dvd style
+            },
+            -- etc
+        })
+    end,
+}
+
+-- makes an error but eh, screw it good enough
+useless["ryoppippi/bad-apple.vim"] = {
+    cmd = "BadApple",
+    requires = { "vim-denops/denops.vim", after = "bad-apple.vim", opt = true },
+    opt = true,
+}
+
 -- your plugin config
 return useless
