3238467bb05e2c49808ea81f3bf9585a2804710e
vsedov <viv.sedov@hotmail.com>
2022-06-12 19:35:41 +0100

    fix luadev not abiding globals

--- lua/modules/lsp/lsp/providers/luadev.lua
+++ lua/modules/lsp/lsp/providers/luadev.lua
@@ -1,60 +1,67 @@
 local enhance_attach = require("modules.lsp.lsp.utils").enhance_attach
-
+local lspconfig = require("lspconfig")
 -- local sumneko_root_path = vim.fn.expand("$HOME") .. "/GitHub/lua-language-server"
 -- local sumneko_binary = vim.fn.expand("$HOME") .. "/GitHub/lua-language-server/bin/lua-language-server"
-local runtime_path = {}
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+-- local runtime_path = {}
+-- table.insert(runtime_path, "lua/?.lua")
+-- table.insert(runtime_path, "lua/?/init.lua")
 local sumneko_lua_server = enhance_attach({
     -- cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
     cmd = { "lua-language-server" },
     settings = {
         Lua = {
-            runtime = {
-                runtime = {
-                    path = runtime_path,
-                    version = "LuaJIT",
+            diagnostics = {
+                globals = {
+                    "vim",
+                    "dump",
+                    "hs",
+                    "lvim",
+                    "describe",
+                    "it",
+                    "before_each",
+                    "after_each",
+                    "pending",
+                    "teardown",
+                    "packer_plugins",
                 },
-                diagnostics = {
-                    enable = true,
-                    globals = {
-                        "vim",
-                        "dump",
-                        "hs",
-                        "lvim",
-                        "describe",
-                        "it",
-                        "before_each",
-                        "after_each",
-                        "pending",
-                        "teardown",
-                        "packer_plugins",
-                    },
+            },
+            completion = { keywordSnippet = "Replace", callSnippet = "Replace" },
+            workspace = {
+                -- remove all of this, as it slows things down
+                library = {
+                    -- vim.api.nvim_get_runtime_file("", true),
+                    -- [table.concat({ vim.fn.stdpath("data"), "lua" }, "/")] = false,
+                    -- vim.api.nvim_get_runtime_file("", false),
+                    -- [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                    -- [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                 },
-                completion = { keywordSnippet = "Replace", callSnippet = "Replace" },
-                workspace = {
-                    -- remove all of this, as it slows things down
-                    library = {
-                        -- vim.api.nvim_get_runtime_file("", true),
-                        -- [table.concat({ vim.fn.stdpath("data"), "lua" }, "/")] = false,
-                        -- vim.api.nvim_get_runtime_file("", false),
-                        -- [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                        -- [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    },
-                    maxPreload = 1000,
-                    preloadFileSize = 10000,
-                },
+                maxPreload = 1000,
+                preloadFileSize = 10000,
             },
         },
     },
 })
+local lua_dev_plugins = {
+    "nvim-notify",
+    "plenary.nvim",
+}
 
+local runtime_path_completion = true
+if not runtime_path_completion then
+    sumneko_lua_server.settings.Lua.runtime = {
+        version = "LuaJIT",
+        path = vim.split(package.path, ";"),
+        vim.fn.expand("~") .. "/.config/nvim//lua/?.lua",
+    }
+end
 local luadev = require("lua-dev").setup({
     library = {
         vimruntime = true,
         types = true,
-        plugins = { "nvim-notify", "plenary.nvim" },
+        plugins = lua_dev_plugins, -- toggle this to get completion for require of all plugins
     },
+    runtime_path = runtime_path_completion,
     lspconfig = sumneko_lua_server,
 })
-require("lspconfig").sumneko_lua.setup(luadev)
+
+lspconfig.sumneko_lua.setup(luadev)
