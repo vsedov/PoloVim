1920c5a63201030c0cd8de990a3893af87cef420
vsedov <viv.sedov@hotmail.com>
2022-07-09 18:01:25 +0100

    update min config

--- min/minconfig.lua
+++ min/minconfig.lua
@@ -13,7 +13,23 @@ local function load_plugins()
                     "nvim-lua/plenary.nvim",
                 },
             },
+            { "nvim-treesitter/nvim-treesitter" },
+            {"rebelot/kanagawa.nvim"},
+
+
+ {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }          ,
+
+
         },
+
         config = {
             package_root = package_root,
             compile_path = install_path .. "/plugin/packer_compiled.lua",
@@ -23,11 +39,67 @@ local function load_plugins()
 end
 
 _G.load_config = function()
+   if not packer_plugins["kanagawa.nvim"].loaded then
+        vim.cmd([[packadd kanagawa.nvim ]])
+    end
+    require("kanagawa").setup({
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = { italic = true },
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        globalStatus = true,
+        typeStyle = {},
+        variablebuiltinStyle = { italic = true },
+        specialReturn = true, -- special highlight for the return keyword
+        specialException = true, -- special highlight for exception handling keywords
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC` -- Kinda messes with things
+        colors = {},
+        overrides = {
+            Pmenu = { fg = "NONE", bg = "NONE" },
+            normalfloat = { bg = "NONE" },
+        },
+    })
+    vim.cmd([[colorscheme kanagawa]])
+
     require("bufferline").setup({
         options = {
             mode = "buffers",
             diagnostics = "nvim_diagnostic",
             always_show_bufferline = false,
+        },
+    })
+    -- require("window-picker").setup()
+    require("neo-tree").setup()
+    require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = { "c", "lua", "python" },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        auto_install = true,
+
+        -- List of parsers to ignore installing (for "all")
+        ignore_install = { "javascript" },
+
+        highlight = {
+            -- `false` will disable the whole extension
+            enable = true,
+
+            -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+            -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+            -- the name of the parser)
+            -- list of language that will be disabled
+            disable = { "c", "rust" },
+
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
         },
     })
 end
