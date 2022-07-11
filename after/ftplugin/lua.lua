9eb054b9e259a2f7a3376d6a3be2d5b4b65f2038
vsedov <viv.sedov@hotmail.com>
2022-07-11 20:39:25 +0100

    yoink

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -52,3 +52,78 @@ end
 vim.keymap.set("n", "gK", keyword, { buffer = 0 })
 vim.opt_local.textwidth = 100
 vim.opt_local.formatoptions:remove("o")
+vim.o.smarttab = true
+
+--- Returns the item that matches the first item in statements
+---@param value any #The value to compare against
+---@param compare? function #A custom comparison function
+---@return function #A function to invoke with a table of potential matches
+local match = function(value, compare)
+    -- Returning a function allows for such syntax:
+    -- match(something) { ..matches.. }
+    return function(statements)
+        if value == nil then
+            return
+        end
+
+        -- Set the comparison function
+        -- A comparison function may be required for more complex
+        -- data types that need to be compared against another static value.
+        -- The default comparison function compares booleans as strings to ensure
+        -- that boolean comparisons work as intended.
+        compare = compare
+            or function(lhs, rhs)
+                if type(lhs) == "boolean" then
+                    return tostring(lhs) == rhs
+                end
+
+                return lhs == rhs
+            end
+
+        -- Go through every statement, compare it, and perform the desired action
+        -- if the comparison was successful
+        for case, action in pairs(statements) do
+            if compare(value, case) then
+                -- The action can be a function, in which case it is invoked
+                -- and the return value of that function is returned instead.
+                if type(action) == "function" then
+                    return action(value)
+                end
+
+                return action
+            end
+        end
+
+        -- If we've fallen through all statements to check and haven't found
+        -- a single match then see if we can fall back to a `_` clause instead.
+        if statements._ then
+            local action = statements._
+
+            if type(action) == "function" then
+                return action(value)
+            end
+
+            return action
+        end
+    end
+end
+local editing = function(options)
+    local function set_options(keys, value)
+        return function()
+            for _, key in ipairs(keys) do
+                vim.opt_local[key] = value
+            end
+        end
+    end
+
+    for key, value in pairs(options) do
+        match(key)({
+            indent = set_options({ "shiftwidth", "tabstop", "softtabstop" }, value),
+            spaces = set_options({ "expandtab" }, value),
+        })
+    end
+end
+editing({
+    indent = 4,
+    spaces = true,
+})
