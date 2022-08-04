af343377bb3787f3fc6dfe8b4a959363cab21b0b
vsedov <viv.sedov@hotmail.com>
2022-08-04 16:34:13 +0100

    vista

--- lua/keymap/useful.lua
+++ lua/keymap/useful.lua
@@ -78,7 +78,8 @@ local plug_map = {
         :with_noremap()
         :with_silent(),
     ---- private peek
-    ["n|<Leader>v"] = map_cu("SymbolsOutline", "Vistaaa"):with_noremap():with_silent(),
+    ["n|<Leader>v"] = map_cu("Vista!!", "Vistaaa"):with_noremap():with_silent(),
+
     ["n|<Leader>gt"] = map_cr(
         '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
         "Git Linker open browser range "
