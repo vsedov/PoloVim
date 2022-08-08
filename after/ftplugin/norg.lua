cd4864f6c996bf999754628cd5514b098617447b
vsedov <viv.sedov@hotmail.com>
2022-08-08 17:03:58 +0100

    fix neorg conceals

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,7 +1,7 @@
 -- https://github.com/max397574/NeovimConfig/blob/master/clipboard_neorg.vim
-vim.bo.shiftwidth = 2
-vim.bo.conceallevel = 2
-vim.bo.concealcursor = "nv"
+vim.o.shiftwidth = 2
+vim.o.conceallevel = 2
+vim.o.concealcursor = "nv"
 
 vim.bo.commentstring = "#%s"
 vim.cmd([[source ~/.config/nvim/clipboard_neorg.vim]])
