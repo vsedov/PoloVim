59e72931b4538bba9f06aec911539f0e1f18d9fb
vsedov <viv.sedov@hotmail.com>
2022-08-20 03:13:28 +0100

    suits.nvim

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -892,6 +892,36 @@ function config.dashboard_config()
     -- }
     --
 end
+function config.suit()
+    require("suit").setup({
+        input = {
+            -- default prompt value
+            default_prompt = "Input: ",
+            -- border of the window (see `:h nvim_open_win`)
+            border = lambda.style.border.type_0,
+            -- highlight group for input
+            hl_input = "NormalFloat",
+            -- highlight group for prompt
+            hl_prompt = "NormalFloat",
+            -- highlight group for window border
+            hl_border = "FloatBorder",
+        },
+        select = {
+            -- default prompt value
+            default_prompt = "Select one of: ",
+            -- border of the window (see `:h nvim_open_win`)
+            border = "single",
+            -- highlight group for select list
+            hl_select = "NormalFloat",
+            -- highlight group for prompt
+            hl_prompt = "NormalFloat",
+            -- highlight group for current selected item
+            hl_selected_item = "PmenuSel",
+            -- highlight group for window border
+            hl_border = "FloatBorder",
+        },
+    })
+end
 
 vim.api.nvim_exec(
     [[
--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -120,7 +120,8 @@ ui({
 
 ui({
     "kevinhwang91/nvim-ufo",
-    ft = lambda.config.main_file_types,
+    --[[ ft = lambda.config.main_file_types, ]]
+    event = "BufEnter",
     requires = { "kevinhwang91/promise-async", after = "nvim-ufo" },
     config = conf.ufo,
 })
@@ -160,34 +161,5 @@ ui({
 ui({
     "doums/suit.nvim",
     event = "BufEnter",
-    config = function()
-        require("suit").setup({
-            input = {
-                -- default prompt value
-                default_prompt = "Input: ",
-                -- border of the window (see `:h nvim_open_win`)
-                border = lambda.style.border.type_0,
-                -- highlight group for input
-                hl_input = "NormalFloat",
-                -- highlight group for prompt
-                hl_prompt = "NormalFloat",
-                -- highlight group for window border
-                hl_border = "FloatBorder",
-            },
-            select = {
-                -- default prompt value
-                default_prompt = "Select one of: ",
-                -- border of the window (see `:h nvim_open_win`)
-                border = "single",
-                -- highlight group for select list
-                hl_select = "NormalFloat",
-                -- highlight group for prompt
-                hl_prompt = "NormalFloat",
-                -- highlight group for current selected item
-                hl_selected_item = "PmenuSel",
-                -- highlight group for window border
-                hl_border = "FloatBorder",
-            },
-        })
-    end,
+    config = conf.suit,
 })
