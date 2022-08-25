044f90f23a2d0e1a167b7749a37d4cef7c8a409f
vsedov <viv.sedov@hotmail.com>
2022-08-25 01:21:52 +0100

    remove temp changes

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
 require("utils.plugins.profiler")
 require("core")
 require("overwrite")
-local test = 10
-print("DEBUGPRINT[2]: init.lua:4 (after local test = 10)")
-print("DEBUGPRINT[1]: init.lua:4: test=" .. vim.inspect(test))
