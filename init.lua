d67108dd6b7e326c15c1e27d5d79b1283811ae57
vsedov <viv.sedov@hotmail.com>
2022-08-25 01:21:43 +0100

    remove temp changes

--- init.lua
+++ init.lua
@@ -4,11 +4,3 @@ require("overwrite")
 local test = 10
 print("DEBUGPRINT[2]: init.lua:4 (after local test = 10)")
 print("DEBUGPRINT[1]: init.lua:4: test=" .. vim.inspect(test))
-
-local function parse_test(test)
-    print('DEBUGPRINT[5]: init.lua:9 (before )')
-    local test = test or {}
-
-    print('DEBUGPRINT[4]: init.lua:10 (after local test = test or {})')
-
-end
