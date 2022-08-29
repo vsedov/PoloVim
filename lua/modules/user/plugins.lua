cd70b468b63d934206b2105df1488c17939e5e54
vsedov <viv.sedov@hotmail.com>
2022-08-29 09:54:42 +0100

    New Plugins

--- lua/modules/user/plugins.lua
+++ lua/modules/user/plugins.lua
@@ -4,17 +4,6 @@ local user = require("core.pack").package
 -- True emotional Support
 user({ "rtakasuke/vim-neko", cmd = "Neko", opt = true })
 
--- Competitive programming
--- user {
--- "nullchilly/cpeditor.nvim",
--- opt = true,
--- config = function()
---     require "config.cpeditor"
--- end,
--- setup = function()
---     lazy "cpeditor.nvim"
--- end,
--- }
 -- TODO(vsedov) (00:11:22 - 13/08/22): Temp plugin
 user({
     "RRethy/vim-illuminate",
