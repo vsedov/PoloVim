59f03399f253773814b0e15398db069e314ba4a0
vsedov <viv.sedov@hotmail.com>
2022-08-31 07:20:36 +0100

    new plugins

--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -222,3 +222,64 @@ tools({
 })
 
 tools({ "ibhagwan/fzf-lua", branch = "main", config = conf.fzf, opt = true, cmd = { "FzfLua" } })
+
+-- Default:~
+--     normal = {
+--       ["<cr>"] = "open_data",
+--       ["<s-cr>"] = "open_data_index",
+--       ["<tab>"] = "toggle_node",
+--       ["<s-tab>"] = "toggle_node",
+--       ["/"] = "select_path",
+--       ["$"] = "change_icon_menu",
+--       c = "add_inside_end_index",
+--       I = "add_inside_start",
+--       i = "add_inside_end",
+--       l = "copy_node_link",
+--       L = "copy_node_link_index",
+--       d = "delete",
+--       O = "add_above",
+--       o = "add_below",
+--       q = "quit",
+--       r = "rename",
+--       R = "change_icon",
+--       u = "make_url",
+--       x = "select",
+--     }
+
+--     selection = {
+--       ["<cr>"] = "open_data",
+--       ["<s-tab>"] = "toggle_node",
+--       ["/"] = "select_path",
+--       I = "move_inside_start",
+--       i = "move_inside_end",
+--       O = "move_above",
+--       o = "move_below",
+--       q = "quit",
+--       x = "select",
+--     }
+
+tools({
+    "phaazon/mind.nvim",
+    cmd = { "MindOpenMain", "MindOpenProject", "MindReloadState" },
+    config = function()
+        require("mind").setup()
+    end,
+})
+
+tools({
+    lambda.use_local("pomodoro.nvim", "personal"),
+    cmd = {
+        "PomodoroStartFocus",
+        "PomodoroStartBreak",
+        "PomodoroStartLongBreak",
+        "PomodoroPause",
+        "PomodoroResume",
+        "PomodoroTogglePopup",
+    },
+})
+
+tools({
+    "mong8se/actually.nvim",
+    wants = "dressing.nvim",
+    event = "BufNewFile",
+})
