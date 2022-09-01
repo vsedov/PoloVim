a3be1116867a82d0a8247d9ec37b9941ee623a4b
vsedov <viv.sedov@hotmail.com>
2022-09-01 07:59:57 +0100

    feat: heirline better scrollbar

--- lua/modules/ui/heirline.lua
+++ lua/modules/ui/heirline.lua
@@ -213,6 +213,20 @@ local FileLastModified = {
     end,
 }
 
+local ScrollBar = {
+    static = {
+        -- sbar = { "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" },
+        sbar = { "🭶", "🭷", "🭸", "🭹", "🭺", "🭻" },
+    },
+    provider = function(self)
+        local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+        local lines = vim.api.nvim_buf_line_count(0)
+        local i = math.floor(curr_line / lines * (#self.sbar - 1)) + 1
+        return string.rep(self.sbar[i], 2)
+    end,
+    hl = { fg = "blue", bg = "bright_bg" },
+}
+
 local Ruler = {
     -- %l = current line number
     -- %L = number of lines in the buffer
