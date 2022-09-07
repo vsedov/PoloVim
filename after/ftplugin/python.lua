145d7b66f845ee43eab7769cd6abb0b5687deecd
vsedov <viv.sedov@hotmail.com>
2022-09-07 11:48:49 +0100

    NeoGit use ftplugin_conf based on filetype

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -7,16 +7,10 @@ local options = {
     colorcolumn = "130",
     foldmethod = "syntax",
 }
+vim.api.nvim_command("setlocal foldexpr=nvim_treesitter#foldexpr()")
 
 for k, v in pairs(options) do
     vim.o[k] = v
-end
-
--- Enter the python virtual environment without having sourced the file before entering neovim
-if vim.fn.exists("$VIRTUAL_ENV") == 1 then
-    vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system("which -a python | head -n2 | tail -n1"), "\n", "", "g")
-else
-    vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system("which python"), "\n", "", "g")
 end
 
 local ts_utils = require("nvim-treesitter.ts_utils")
