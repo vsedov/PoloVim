7950288e4e13ee9a9fb0868e6532c63967a250e5
vsedov <viv.sedov@hotmail.com>
2022-09-17 00:31:54 +0100

    remove telescope folder

--- lua/modules/telescope/config.lua
+++
@@ -1,9 +0,0 @@
-local config = {}
-
-function config.telescope()
-    require("utils.telescope").setup()
-end
-function config.bookmark()
-    require("telescope").load_extension("bookmarks")
-end
-return config
