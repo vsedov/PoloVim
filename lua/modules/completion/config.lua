2c73a9157abf12cb6e9806c0fc5ea6733dc42e81
vsedov <viv.sedov@hotmail.com>
2022-09-17 00:29:41 +0100

    refactor(tabnine): refactor tabnine , use new method in readme

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -19,16 +19,15 @@ end
 
 function config.tabnine()
     local tabnine = require("cmp_tabnine.config")
-    tabnine:setup({
+    tabnine.setup({
         max_lines = 1000,
         max_num_results = 20,
         sort = true,
         run_on_every_keystroke = true,
-        snippet_placeholder = "...",
-        ignored_file_types = { -- default is not to ignore
-            -- uncomment to ignore in lua:
-            -- lua = true
-            norg = "true",
+        snippet_placeholder = lambda.style.icons.misc.ellipsis,
+        ignored_file_types = {
+
+            norg = true,
         },
         show_prediction_strength = true,
     })
