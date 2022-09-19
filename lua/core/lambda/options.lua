36314970922d2d07687d62670291d7f0ddff8aea
vsedov <viv.sedov@hotmail.com>
2022-09-20 00:09:59 +0100

    modularise options.lua

--- lua/core/lambda/options.lua
+++ lua/core/lambda/options.lua
@@ -1,54 +1,11 @@
 -- pick random  item form dark but based on its probability
 lambda.config = {
-    leader = {
-        -- TODO: (vsedov) (21:18:41 - 03/09/22): My entire keymap config
-        -- will need to be refactored , soon , so to do so , im preping in advice:
-        -- i might get rid of this though , im not sure.
-        leader = "<leader>",
-        local_leader = "<localleader",
-        leader_s_colon = ";",
-        leader_c_space = "<C-<leader>>",
-    },
-    colourscheme = {
-        dim_background = false,
-        change_kitty_bg = false,
-        --- @usage "main"' | '"moon"
-        rose = "main",
-        --- @usage  "latte" | "frappe" | "macchiato" | "mocha"
-        catppuccin_flavour = "mocha",
-        -- @usage theme_name : percentage chance
-        themes = {
-            dark = {
-                "kanagawa.nvim",
-                "rose",
-                "catppuccin",
-                -- "horizon.nvim",
-                -- "vim-dogrun",
-                "nvim-tundra",
-                -- "doom-one.nvim",
-                "tokyonight.nvim",
-            },
-        },
-    },
     do_you_want_lag = false, -- Enable Extra regex,
     telescope_theme = "float_all_borders", -- custom_bottom_no_borders float_all_borders
-    -- TODO: (vsedov) (09:58:51 - 31/08/22): Use simple notify based on filetpe or project
-    -- and use record your self based on if you are in multiple projects or not , as i think
-    -- this also causes lag when codoing which can get very annoying
     simple_notify = false, -- notifier.nvim = true , else use nvim-notif
     record_your_self = false, -- waka time
     neorg_auto_commit = false,
     loaded_confirm_quit = true,
-    cmp = {
-        tabnine = {
-            use_tabnine = true,
-            tabnine_sort = true, -- I am not sure how i feel about if i want tabnine to actively sort stuff for me.
-            tabnine_prefetch = true,
-        },
-
-        use_rg = false,
-        cmp_theme = "extra", --- @usage "border" | "no-border" | "extra"
-    },
     save_clipboard_on_exit = true,
     abbrev = {
         enable = true,
@@ -80,20 +37,55 @@ lambda.config = {
     use_saga_maps = true, -- Like lspsaga definition or something, or code actions ...
     use_guess_indent = true,
     use_gitsigns = true,
-    lsp = {
-        use_lsp_signature = false,
-        latex = "texlab", -- texlab | ltex
-        python = {
-            lint = { "flake8" }, -- pylint, pyflake, and other linters
-            format = { "isort", "yapf" }, -- black
-            -- jedis documentation is better though
-            lsp = "jedi", -- jedi pylsp and pyright pylance
-            pylance_pyright = {
-                use_inlay_hints = false,
+    use_hlchunk = true,
+    main_file_types = { "python", "norg", "tex", "lua", "c", "cpp", "rust" },
+}
+lambda.config.colourscheme = {
+    dim_background = false,
+    change_kitty_bg = false,
+    --- @usage "main"' | '"moon"
+    rose = "main",
+    --- @usage  "latte" | "frappe" | "macchiato" | "mocha"
+    catppuccin_flavour = "mocha",
+    -- @usage theme_name : percentage chance
+    themes = {
+        dark = {
+            core_themes = {
+                "kanagawa.nvim",
+                "rose",
+                "catppuccin",
+                "tokyonight.nvim",
+                "nvim-tundra",
+            },
+            others = {
+                "horizon.nvim",
+                "vim-dogrun",
+                "doom-one.nvim",
             },
         },
     },
-    main_file_types = { "python", "norg", "tex", "lua", "c", "cpp", "rust" },
+}
+lambda.config.cmp = {
+    tabnine = {
+        use_tabnine = true,
+        tabnine_sort = true, -- I am not sure how i feel about if i want tabnine to actively sort stuff for me.
+        tabnine_prefetch = true,
+        tabnine_priority = 9,
+    },
+    use_rg = false,
+    cmp_theme = "extra", --- @usage "border" | "no-border" | "extra"
 }
 
--- Complte refactor for how python might be loaded
+lambda.config.lsp = {
+    use_lsp_signature = false,
+    latex = "texlab", -- texlab | ltex
+    python = {
+        lint = { "flake8" }, -- pylint, pyflake, and other linters
+        format = { "isort", "yapf" }, -- black
+        -- jedis documentation is better though
+        lsp = "jedi", -- jedi pylsp and pyright pylance
+        pylance_pyright = {
+            use_inlay_hints = false,
+        },
+    },
+}
