56a2395dfa7d01cbe2ffed297bae32f3eb9df58d
vsedov <viv.sedov@hotmail.com>
2022-09-29 06:46:15 +0100

    remove hydra command-t

--- lua/modules/editor/hydra/extra_search.lua
+++ lua/modules/editor/hydra/extra_search.lua
@@ -57,47 +57,12 @@ end
 --  Ive been lazy and havent been bothered with it.
 local function fzy_hydra() end
 
-local function command_t_hydra()
-    hint = [[
-^^^^                  Command_T                 ^^^^
-^^^^▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔^^^^
-_f_: FindFile         _b_: Buffers
-_g_: Git              _r_: RG
-_<Enter>_: CommandT
-^^^^▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔^^^^
-]]
-    loader("command-t")
-    Hydra({
-        name = "Command_T",
-        hint = hint,
-        config = {
-            color = "pink",
-            invoke_on_body = true,
-            hint = {
-                position = "bottom-right",
-                border = lambda.style.border.type_0,
-            },
-        },
-        mode = { "n" },
-        body = "<leader>C",
-        heads = {
-            { "<Enter>", cmd("CommandT"), { exit = false } },
-            { "f", cmd("CommandTFind"), { exit = false } },
-            { "r", cmd("CommandTRipgrep"), { exit = false } },
-            { "g", cmd("CommandTGit"), { exit = false } },
-            { "b", cmd("CommandTBuffer"), { exit = false } },
-            { "<Esc>", nil, { exit = true, desc = false } },
-        },
-    })
-end
-
 when(lambda.config.extra_search.enable, function()
     for name, enable in pairs(lambda.config.extra_search.providers) do
         if enable == true then
             match(name)({
                 use_azy = azy_hydra(),
                 use_fzf_lua = fzy_hydra(),
-                use_command_t = command_t_hydra(),
             })
         end
     end
