419c436dad4d8bda6405d8047ec1e3106831c244
vsedov <viv.sedov@hotmail.com>
2022-10-01 16:58:04 +0100

    feat(goyo): Add goyo

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -544,6 +544,46 @@ editor({
     end,
 })
 
+
+editor({
+    "junegunn/goyo.vim",
+    opt = true, 
+    cmd = {
+        "Goyo"
+    },
+    config = function()
+        vim.g.goyo_width = '85%' -- Default width
+        vim.g.goyo_height = '90%' -- Default height
+
+        vim.cmd [[
+        function! s:goyo_enter()
+          " Hides mode from showing
+          set noshowmode 
+          " Hides the sign column
+          :set scl=no 
+          " Hides lualine
+          lua require"lualine".hide() 
+          " ...
+        endfunction
+        function! s:goyo_leave()
+          " Resets syntax highlighting (workaround for goyo bug)
+          syntax off 
+          syntax on 
+          " Makes the signcolumn match the background colorscheme
+          highlight clear SignColumn
+          " Brings mode back
+          set showmode 
+          " Shows lualine again
+          lua require"lualine".hide({unhide=true})
+          " ...
+        endfunction
+        autocmd! User GoyoEnter nested call <SID>goyo_enter()
+        autocmd! User GoyoLeave nested call <SID>goyo_leave()
+        ]]
+
+    end
+})
+
 editor({
     "marklcrns/vim-smartq",
     keys = {
@@ -559,7 +599,7 @@ editor({
         "SmartQCloseSplits",
     },
     config = function()
-        vim.g.smartq_goyo_integration = 0
+        vim.g.smartq_goyo_integration = 1
         vim.g.smartq_zenmode_integration = 0
     end,
 })
