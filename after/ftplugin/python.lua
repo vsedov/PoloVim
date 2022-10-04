81665f3d93eabdcbec0f5f2c79ba7a92237da6ba
vsedov <viv.sedov@hotmail.com>
2022-10-04 11:29:59 +0100

    remove toggle block markers in python, causes lag

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -47,51 +47,6 @@ local toggle_fstring = function()
     end
     vim.api.nvim_win_set_cursor(winnr, cursor)
 end
-local function toggle_block_markers()
-    local ns_id = vim.api.nvim_create_namespace("bmark")
-
-    if #vim.api.nvim_buf_get_extmarks(0, ns_id, 0, -1, {}) > 0 then
-        if lambda.config.lsp.python.toggle_block_master then
-            vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
-            return
-        end
-    end
-
-    local language = "python"
-    local bufnr = vim.fn.bufnr("%")
-
-    local language_tree = vim.treesitter.get_parser(bufnr, language)
-    local syntax_tree = language_tree:parse()
-    local root = syntax_tree[1]:root()
-
-    local query_template = "((%s) @capture (#offset! @capture))"
-    local params_t = {
-        func = { target = "function_definition", marker = string.rep("~", 100) },
-        decofunc = { target = "decorated_definition", marker = string.rep("~", 100) },
-        class = { target = "class_definition", marker = string.rep("#", 100) },
-    }
-
-    for _, params in pairs(params_t) do
-        local query = vim.treesitter.parse_query(language, string.format(query_template, params.target))
-
-        for _, _, metadata in query:iter_matches(root, bufnr) do
-            line_num = metadata[1].range[1] - 1
-
-            -- make sure there is no text on that line already
-            if #vim.filetype.getlines(bufnr, line_num + 1) == 0 then
-                local opts = {
-                    end_line = line_num,
-                    id = line_num,
-                    virt_text = { { params.marker, "Comment" } },
-                    virt_text_pos = "overlay",
-                }
-
-                -- Add virtual line: https://jdhao.github.io/2021/09/09/nvim_use_virtual_text/
-                vim.api.nvim_buf_set_extmark(0, ns_id, line_num, 0, opts)
-            end
-        end
-    end
-end
 
 local switch_definitions = [[
   let b:switch_custom_definitions = [
@@ -139,14 +94,3 @@ end, { noremap = true })
 
 vim.g.magma_automatically_open_output = false
 vim.g.magma_image_provider = "kitty"
-
-lambda.augroup("PythonBlock", {
-    {
-        -- don't execute silently in case of errors
-        event = { "BufWritePost" },
-        pattern = "*.py",
-        command = function()
-            toggle_block_markers()
-        end,
-    },
-})
