edbdbbaf71c6e540dc2edea06e2c66d652f12b04
vsedov <viv.sedov@hotmail.com>
2022-10-28 06:09:38 +0100

    feat(updoc): currently broken but i like teh idea behind it

--- lua/modules/documentation/plugins.lua
+++ lua/modules/documentation/plugins.lua
@@ -18,12 +18,16 @@ docs({
     cmd = { "RefCopy", "RefGo" },
 })
 
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
 docs({
     "max397574/dyn_help.nvim",
     opt = true,
     keys = "<leader>hw",
     config = conf.dyn_help,
 })
+
 docs({
     "amrbashir/nvim-docs-view",
     opt = true,
@@ -43,4 +47,28 @@ docs({
     "romainl/vim-devdocs",
     cmd = { "DD" },
     opt = true,
+})
+
+--  TODO: (vsedov) (06:07:10 - 28/10/22): Make hydra so this would be nice to use.
+docs({
+    "loganswartz/updoc.nvim",
+    requires = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "MunifTanjim/nui.nvim",
+    },
+    opt = true,
+    cmd = {
+        "USearch",
+        "ULookUp",
+        "UHoverLink",
+    },
+
+    config = function()
+        local udoc = require("updoc")
+        udoc.setup()
+        lambda.command("USearch", udoc.search, {})
+        lambda.command("ULookUp", udoc.lookup, {})
+        lambda.command("UHoverLink", udoc.show_hover_links, {})
+    end,
 })
