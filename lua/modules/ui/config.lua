9b08f14d1dc1f556398eed57fc24958f56836c13
vsedov <viv.sedov@hotmail.com>
2022-11-24 15:58:14 +0000

    use cmp over nui for noice

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -839,7 +839,7 @@ function config.noice()
         popupmenu = {
             enabled = true, -- enables the Noice popupmenu UI
             ---@type 'nui'|'cmp'
-            backend = "nui", -- backend to use to show regular cmdline completions
+            backend = "cmp", -- backend to use to show regular cmdline completions
             ---@type NoicePopupmenuItemKind|false
             -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
             kind_icons = {}, -- set to `false` to disable icons
