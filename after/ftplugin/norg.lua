40ffdb85da6527ea3dcab0b0aa99bf66c91871ce
vsedov <viv.sedov@hotmail.com>
2023-01-20 08:55:39 +0000

    remove neorg cmp core

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -4,22 +4,4 @@ vim.o.conceallevel = 2
 vim.o.concealcursor = "nv"
 
 vim.bo.commentstring = "#%s"
-vim.cmd([[source ~/.config/nvim/clipboard_neorg.vim]])
 vim.cmd([[ Lazy load nvim-treesitter]])
-
--- Not sure if this needs to be on or not
--- vim.o.spell = 1
-
--- wait till norg is fixed
-
-local neorg = require("neorg")
-local function load_completion()
-    neorg.modules.load_module("core.norg.completion", nil, {
-        engine = "nvim-cmp",
-    })
-end
-if neorg.is_loaded() then
-    load_completion()
-else
-    neorg.callbacks.on_event("core.started", load_completion)
-end
