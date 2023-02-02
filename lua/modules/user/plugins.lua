9e3ff40c72877e61714211cb939e6afd2f2036d0
vsedov <viv.sedov@hotmail.com>
2023-02-02 14:48:20 +0000

    better oil setup

--- lua/modules/user/plugins.lua
+++ lua/modules/user/plugins.lua
@@ -70,63 +70,61 @@ user({
     init = function()
         vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
     end,
-    config = function()
-        require("oil").setup({
-            columns = {
-                "icon",
-                -- "permissions",
-                "mtime",
-            },
-            buf_options = {
-                buflisted = true,
-            },
-            -- Window-local options to use for oil buffers
+    opts = {
+        columns = {
+            "icon",
+            -- "permissions",
+            "mtime",
+        },
+        buf_options = {
+            buflisted = true,
+        },
+        -- Window-local options to use for oil buffers
+        win_options = {
+            wrap = true,
+            signcolumn = "no",
+            cursorcolumn = false,
+            foldcolumn = "0",
+            spell = false,
+            list = false,
+            conceallevel = 3,
+            concealcursor = "n",
+        },
+        -- Restore window options to previous values when leaving an oil buffer
+        restore_win_options = true,
+        -- Skip the confirmation popup for simple operations
+        skip_confirm_for_simple_edits = false,
+        keymaps = {
+            ["g?"] = "actions.show_help",
+            ["<CR>"] = "actions.select",
+            ["<C-s>"] = "actions.select_vsplit",
+            ["<C-h>"] = "actions.select_split",
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = "actions.close",
+            ["<C-l>"] = "actions.refresh",
+            ["-"] = "actions.parent",
+            ["_"] = "actions.open_cwd",
+            ["c"] = "actions.cd",
+            ["C"] = "actions.tcd",
+            ["g."] = "actions.toggle_hidden",
+        },
+        -- Set to false to disable all of the above keymaps
+        use_default_keymaps = false,
+        view_options = {
+            show_hidden = false,
+        },
+        -- Configuration for the floating window in oil.open_float
+        float = {
+            -- Padding around the floating window
+            padding = 2,
+            max_width = 0,
+            max_height = 0,
+            border = "rounded",
             win_options = {
-                wrap = true,
-                signcolumn = "no",
-                cursorcolumn = false,
-                foldcolumn = "0",
-                spell = false,
-                list = false,
-                conceallevel = 3,
-                concealcursor = "n",
+                winblend = 10,
             },
-            -- Restore window options to previous values when leaving an oil buffer
-            restore_win_options = true,
-            -- Skip the confirmation popup for simple operations
-            skip_confirm_for_simple_edits = false,
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["<C-s>"] = "actions.select_vsplit",
-                ["<C-h>"] = "actions.select_split",
-                ["<C-p>"] = "actions.preview",
-                ["<C-c>"] = "actions.close",
-                ["<C-l>"] = "actions.refresh",
-                ["-"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["c"] = "actions.cd",
-                ["C"] = "actions.tcd",
-                ["g."] = "actions.toggle_hidden",
-            },
-            -- Set to false to disable all of the above keymaps
-            use_default_keymaps = false,
-            view_options = {
-                show_hidden = false,
-            },
-            -- Configuration for the floating window in oil.open_float
-            float = {
-                -- Padding around the floating window
-                padding = 2,
-                max_width = 0,
-                max_height = 0,
-                border = "rounded",
-                win_options = {
-                    winblend = 10,
-                },
-            },
-        })
-    end,
+        },
+    },
 })
 
 user({
@@ -341,39 +339,45 @@ user({
 
     "strash/everybody-wants-that-line.nvim",
     event = "VeryLazy",
-    config = function()
-        require("everybody-wants-that-line").setup({
-            buffer = {
-                enabled = true,
-                prefix = "λ:",
-                symbol = "0",
-                max_symbols = 5,
-            },
-            diagnostics = {
-                enabled = true,
-            },
-            quickfix_list = {
-                enabled = true,
-            },
-            git_status = {
-                enabled = true,
-            },
-            filepath = {
-                enabled = true,
-                path = "relative",
-                shorten = false,
-            },
-            filesize = {
-                enabled = true,
-                metric = "decimal",
-            },
-            ruller = {
-                enabled = true,
-            },
-            filename = {
-                enabled = true,
-            },
-            separator = "│",
-        })
-    end,
+    opts = {
+        buffer = {
+            enabled = true,
+            prefix = "λ:",
+            symbol = "0",
+            max_symbols = 5,
+        },
+        diagnostics = {
+            enabled = true,
+        },
+        quickfix_list = {
+            enabled = true,
+        },
+        git_status = {
+            enabled = true,
+        },
+        filepath = {
+            enabled = true,
+            path = "relative",
+            shorten = false,
+        },
+        filesize = {
+            enabled = true,
+            metric = "decimal",
+        },
+        ruller = {
+            enabled = true,
+        },
+        filename = {
+            enabled = false,
+        },
+        separator = "│",
+    },
+})
+
+user({
+
+    "mskelton/local-yokel.nvim",
+    lazy = true,
+    cmd = { "E" },
+    config = true,
 })
