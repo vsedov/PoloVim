7878712af8047a1fe14e0174d627c0997f37a47d
vsedov <viv.sedov@hotmail.com>
2023-02-04 15:14:49 +0000

    move overseer changes

--- lua/modules/lang/overseer/binds.lua
+++ lua/modules/lang/overseer/binds.lua
@@ -1,6 +1,6 @@
 local previous_cmd = ""
-
-vim.api.nvim_create_user_command("T", function(param)
+local overseer = require("overseer")
+local ft = vim.api.nvim_create_user_command("T", function(param)
     vim.cmd("OverseerOpen!")
     param = param or "python %"
     vim.cmd("OverseerRunCmd " .. param)
@@ -18,7 +18,24 @@ vim.keymap.set("n", "_w", function()
 end, { noremap = true, silent = true })
 
 vim.api.nvim_create_user_command("Tp", function(param)
-    vim.cmd("OverseerOpen")
-    vim.cmd("OverseerRunCmd python %")
-    previous_cmd = "python %"
+    -- vim.cmd("OverseerOpen")
+    command = "Run " .. vim.bo.filetype:gsub("^%l", string.upper) .. " file (" .. vim.fn.expand("%:t") .. ")"
+    overseer.run_template({ name = command }, function(task)
+        if task then
+            overseer.run_action(task, "open")
+        end
+    end)
 end, { nargs = "?", force = true })
+
+vim.api.nvim_create_user_command("WatchFormat", function()
+    overseer.run_template({ name = "PythonFormat" }, function(task)
+        if task then
+            -- task:add_component({ "format_on_save", path = vim.fn.expand("%:p") })
+            local main_win = vim.api.nvim_get_current_win()
+            overseer.run_action(task, "open float")
+            vim.api.nvim_set_current_win(main_win)
+        else
+            vim.notify("WatchFormat not supported for filetype " .. vim.bo.filetype, vim.log.levels.ERROR)
+        end
+    end)
+end, {})
