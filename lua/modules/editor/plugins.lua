b96a355496a6a4e9b8182e4477f5d01d2b6523af
vsedov <viv.sedov@hotmail.com>
2023-02-11 07:40:36 +0000

    fix wordmotion issue

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -87,8 +87,21 @@ editor({
 -- -- --[[ This thing causes issues with respect to cmdheight=0 ]]
 editor({
     "chaoren/vim-wordmotion",
-    lazy = true,
-    event = "CursorMoved",
+    event = "BufEnter",
+    config = function()
+        vim.g.wordmotion_nomap = 1
+        for _, key in ipairs({"w", "W", "b", "B", "e", "E", "ge", "gE"}) do
+            vim.keymap.set({ "n", "x", "o" }, key, "<Plug>WordMotion_" .. key)
+        end
+
+        vim.keymap.set({ "x", "o" }, "aW", "<Plug>WordMotion_aW")
+        vim.keymap.set({ "x", "o" }, "aw", "<Plug>WordMotion_aw")
+        vim.keymap.set({ "x", "o" }, "iW", "<Plug>WordMotion_iW")
+        vim.keymap.set({ "x", "o" }, "iw", "<Plug>WordMotion_iw")
+
+        vim.keymap.set("c", "<C-R><C-W>", "<Plug>WordMotion_<C-R><C-W>")
+        vim.keymap.set("c", "<C-R><C-A>", "<Plug>WordMotion_<C-R><C-A>")
+  end
 })
 
 editor({
