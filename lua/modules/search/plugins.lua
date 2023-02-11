539dd9e78e0e2aa78462e5dbe829c57365a6e8f3
vsedov <viv.sedov@hotmail.com>
2023-02-11 07:40:53 +0000

    add tele lazy

--- lua/modules/search/plugins.lua
+++ lua/modules/search/plugins.lua
@@ -19,6 +19,35 @@ search({
             "nvim-telescope/telescope-live-grep-args.nvim",
             lazy = true,
         },
+        {
+            "tsakirist/telescope-lazy.nvim",
+            lazy = true,
+            config = function()
+                require("telescope").setup({
+                    extensions = {
+                        lazy = {
+                            -- Optional theme (the extension doesn't set a default theme)
+                            theme = "ivy",
+                            -- Whether or not to show the icon in the first column
+                            show_icon = true,
+                            -- Mappings for the actions
+                            mappings = {
+                                open_in_browser = "<C-o>",
+                                open_in_file_browser = "<M-b>",
+                                open_in_find_files = "<C-f>",
+                                open_in_live_grep = "<C-g>",
+                                open_plugins_picker = "<C-b>", -- Works only after having called first another action
+                                open_lazy_root_find_files = "<C-r>f",
+                                open_lazy_root_live_grep = "<C-r>g",
+                            },
+                            -- Other telescope configuration options
+                        },
+                    },
+                })
+
+                require("telescope").load_extension("lazy")
+            end,
+        },
     },
     config = conf.telescope,
 })
