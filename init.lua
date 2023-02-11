ccc91b0c7ed8a8cae707420ed2adde9b57fc91a2
vsedov <viv.sedov@hotmail.com>
2023-02-11 07:41:19 +0000

    remove dead data

--- init.lua
+++ init.lua
@@ -1,6 +1,2 @@
 require("core")
 require("modules.lsp.lsp.config.handlers").setup()
-
-vim.keymap.set({ "i", "c", "n" }, "<C-g>c", "<Plug>CapsLockToggle")
-vim.keymap.set("i", "<C-l>", "<Plug>CapsLockToggle")
-
