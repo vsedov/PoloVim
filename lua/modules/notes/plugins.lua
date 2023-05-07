4c8d72f2167b8cf53850c1a5c5da8cefcf139f8c
vsedov <viv.sedov@hotmail.com>
2023-05-07 14:08:00 +0100

    Feat: Reintroduce norg plugins.

--- lua/modules/notes/plugins.lua
+++ lua/modules/notes/plugins.lua
@@ -1,34 +1,33 @@
 local conf = require("modules.notes.config")
 local notes = require("core.pack").package
 
--- notes({
---     "nvim-neorg/neorg",
---     -- lazy = true,
---     -- ft = "norg",
---     -- cmd = "Neorg",
---     opts = require("modules.notes.neorg").opts,
--- })
---
--- notes({
---     "AckslD/nvim-FeMaco.lua",
---     lazy = true,
---     ft = { "norg", "markdown", "tex" },
---     cmd = "FeMaco",
---     config = conf.femaco,
--- })
---
--- notes({
---     "jubnzv/mdeval.nvim",
---     lazy = true,
---     ft = { "norg" },
---     config = conf.mdeval,
--- })
---
--- notes({
---     "dhruvasagar/vim-table-mode",
---     lazy = true,
---     cmd = "TableModeToggle",
---     ft = { "norg", "markdown" },
---     conf = conf.table,
--- })
---
+notes({
+    "nvim-neorg/neorg",
+    -- lazy = true,
+    -- ft = "norg",
+    -- cmd = "Neorg",
+    opts = require("modules.notes.neorg").opts,
+})
+
+notes({
+    "AckslD/nvim-FeMaco.lua",
+    lazy = true,
+    ft = { "norg", "markdown", "tex" },
+    cmd = "FeMaco",
+    config = conf.femaco,
+})
+
+notes({
+    "jubnzv/mdeval.nvim",
+    lazy = true,
+    ft = { "norg" },
+    config = conf.mdeval,
+})
+
+notes({
+    "dhruvasagar/vim-table-mode",
+    lazy = true,
+    cmd = "TableModeToggle",
+    ft = { "norg", "markdown" },
+    conf = conf.table,
+})
