c5ef4017123ef078cfad87be82f1abe81416eb90
vsedov <viv.sedov@hotmail.com>
2023-05-30 07:57:17 +0100

    Lambda: change hover and remove heirline config

--- lua/core/lambda/options.lua
+++ lua/core/lambda/options.lua
@@ -142,7 +142,7 @@ lambda.config.cmp = {
 }
 
 lambda.config.lsp = {
-    use_hover = true,
+    use_hover = false, -- Saga is better for this one
     use_lsp_lines = true,
     use_lsp_signature = true,
     use_typos = true,
@@ -167,10 +167,10 @@ lambda.config.lsp = {
 lambda.config.ui = {
     use_illuminate = true,
     use_murmur = false, -- this causes issues with my yanky config -
-    use_heirline = false,
+    use_heirline = true,
     use_ufo = true, --  REVISIT: (vsedov) (03:43:35 - 16/11/22): Come back to this
     use_scroll = true,
-    use_tint = true,
+    use_tint = false,
     use_pet = false,
     use_virtcol = true,
     use_indent_blankline = true,
@@ -179,14 +179,6 @@ lambda.config.ui = {
         lsp = {
             use_noice_signature = false, -- I would very much like to use this,l but for now this is broken
             use_noice_hover = false,
-        },
-    },
-    heirline = {
-        cava = {
-            use_cava = false,
-            fps = "240",
-            bars = "30",
-            audio = "stereo", --average, stero left right
         },
     },
     flirt = {
