f0f9d71fde4c0284e217eaab3af9c56caf4303e3
vsedov <viv.sedov@hotmail.com>
2023-06-02 08:56:30 +0100

    Feat: Nice Keybind for brackets.

--- lua/keymap/insert.lua
+++ lua/keymap/insert.lua
@@ -7,5 +7,6 @@ local plug_map = {
     -- ["i|lm"] = map_cmd("{}<esc>i", "{}"):with_silent(),
     --
     ["i|<c-q>"] = map_cmd([[<esc>:call search("[)\\]}>,`'\"]", 'eW')<CR>]], "Jump Brackets"):with_silent(),
+    ["i|<c-BS>"] = map_cmd("<esc>cvb", "Jump Brackets"):with_silent(),
 }
 return plug_map
