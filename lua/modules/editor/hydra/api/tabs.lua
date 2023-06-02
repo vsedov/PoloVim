60a61d5e777067af12b0f2740ded76cabc2b02e9
vsedov <viv.sedov@hotmail.com>
2023-06-02 05:01:19 +0100

    Hydra: Updated, Tabs, Exectutor modules + new tabs modules.

---
+++ lua/modules/editor/hydra/api/tabs.lua
@@ -0,0 +1,67 @@
+local leader_key = "<leader>B"
+local config = {
+    Tab = {
+        color = "blue",
+        body = leader_key,
+        mode = { "n" },
+        n = {
+            function()
+                vim.cmd("tabnew")
+            end,
+            { desc = "New tab", exit = true },
+        },
+        c = {
+            function()
+                vim.cmd("tabclose")
+            end,
+            { desc = "Close tab", exit = true },
+        },
+        l = {
+            function()
+                vim.cmd("tabnext")
+            end,
+            { desc = "Next tab", exit = false },
+        },
+        h = {
+            function()
+                vim.cmd("tabprev")
+            end,
+            { desc = "Previous tab", exit = false },
+        },
+        f = {
+            function()
+                vim.cmd("tabfirst")
+            end,
+            { desc = "First tab", exit = false },
+        },
+        L = {
+            function()
+                vim.cmd("tablast")
+            end,
+            { desc = "Last tab", exit = false },
+        },
+        m = {
+            function()
+                vim.ui.input({ prompt = "Move tab to:" }, function(idx)
+                    idx = idx and tonumber(idx)
+                    if idx then
+                        vim.cmd("tabmove " .. idx)
+                    end
+                end)
+            end,
+            { desc = "Move tab", exit = true },
+        },
+        ["<ESC>"] = { nil, { desc = "Exit", exit = true } },
+    },
+}
+
+return {
+    config,
+    "Tab",
+    {
+        { "f", "L", "m" },
+    },
+    { "n", "c", "l", "h" },
+    6,
+    3,
+}
