875a78e320037fa40f3875e293a52ea2ac0d7eae
vsedov <viv.sedov@hotmail.com>
2023-07-13 19:46:02 +0100

    feat: buf changes

--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -261,6 +261,26 @@ tools({
                 end
             end,
         },
+        block_for = {
+            gitcommit = true,
+            gitrebase = true,
+        },
+        post_open = function(bufnr, winnr, ft, is_blocking)
+            vim.w[winnr].is_remote = true
+            if is_blocking then
+                vim.bo.bufhidden = "wipe"
+                vim.api.nvim_create_autocmd("BufHidden", {
+                    desc = "Close window when buffer is hidden",
+                    callback = function()
+                        if vim.api.nvim_win_is_valid(winnr) then
+                            vim.api.nvim_win_close(winnr, true)
+                        end
+                    end,
+                    buffer = bufnr,
+                    once = true,
+                })
+            end
+        end,
     },
 })
 
