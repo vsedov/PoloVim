b7f594d114d727e3c8b58c100fa0b52e3a0cedaa
vsedov <viv.sedov@hotmail.com>
2023-10-30 09:57:10 +0000

    use lazy load for smart windows

--- lua/modules/windows/plugins.lua
+++ lua/modules/windows/plugins.lua
@@ -9,8 +9,8 @@ windows({
 
 windows({
     "mrjones2014/smart-splits.nvim",
+    event = "VeryLazy",
     cond = lambda.config.windows.use_smart_splits,
-    lazy = true,
     build = "./kitty/install-kittens.bash",
     dependencies = {
         {
@@ -19,7 +19,7 @@ windows({
                 local opts = { noremap = true, silent = true }
                 require("bufresize").setup({
                     register = {
-    keys = {
+                        keys = {
                             { "n", "<C-w><", "<C-w><", opts },
                             { "n", "<C-w>>", "<C-w>>", opts },
                             { "n", "<C-w>+", "<C-w>+", opts },
@@ -48,22 +48,86 @@ windows({
                     on_leave = require("bufresize").register,
                 },
             },
-            extensions = {
-                -- default settings shown below:
-                smart_splits = {
-                    directions = { "h", "j", "k", "l" },
-                    mods = {
-                        -- for moving cursor between windows
-                        move = "<C>",
-                        -- for resizing windows
-                        resize = "<M>",
-                        -- for swapping window buffers
-                        swap = false, -- false disables creating a binding
-                    },
-                },
-            },
         })
-            end,
+        local keys = {
+            {
+                "<A-h>",
+                function()
+                    require("smart-splits").resize_left()
+                end,
+                desc = "Resize left",
+            },
+            {
+                "<A-l>",
+                function()
+                    require("smart-splits").resize_right()
+                end,
+                desc = "Resize right",
+            },
+            -- moving between splits
+            {
+                "<C-h>",
+                function()
+                    require("smart-splits").move_cursor_left()
+                end,
+                desc = "move left",
+            },
+            {
+                "<C-j>",
+                function()
+                    require("smart-splits").move_cursor_down()
+                end,
+                desc = "move down",
+            },
+
+            {
+                "<C-k>",
+                function()
+                    require("smart-splits").move_cursor_up()
+                end,
+                desc = "move up",
+            },
+            {
+                "<C-l>",
+                function()
+                    require("smart-splits").move_cursor_right()
+                end,
+                desc = "move right",
+            },
+            -- swapping buffers between windows
+            {
+                "<leader><leader>h",
+                function()
+                    require("smart-splits").swap_buf_left()
+                end,
+                desc = "swap left",
+            },
+            {
+                "<leader><leader>j",
+                function()
+                    require("smart-splits").swap_buf_down()
+                end,
+                desc = "swap down",
+            },
+            {
+                "<leader><leader>k",
+                function()
+                    require("smart-splits").swap_buf_up()
+                end,
+                desc = "swap up",
+            },
+            {
+                "<leader><leader>l",
+                function()
+                    require("smart-splits").swap_buf_right()
+                end,
+                desc = "swap right",
+            },
+        }
+        for _, key in ipairs(keys) do
+            vim.keymap.set("n", key[1], key[2], { noremap = true, silent = true })
+        end
+    end,
 })
 
 windows({
