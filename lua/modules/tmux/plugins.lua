be053ce0cac6fbcbf3452d48889c7de02c075adb
vsedov <viv.sedov@hotmail.com>
2023-12-10 15:57:05 +0000

    disable some tmux plugins temp; testing a tmux + kitty setup

--- lua/modules/tmux/plugins.lua
+++ lua/modules/tmux/plugins.lua
@@ -2,14 +2,14 @@ local tmux = require("core.pack").package
 
 tmux({
     "wincent/terminus",
-    cond = vim.fn.getenv("TMUX") ~= vim.NIL,
+    cond = vim.fn.getenv("TMUX") ~= vim.NIL and false,
     event = "VeryLazy",
 })
 
 tmux({
     "aserowy/tmux.nvim",
     lazy = true,
-    cond = vim.fn.getenv("TMUX") ~= vim.NIL,
+    cond = vim.fn.getenv("TMUX") ~= vim.NIL and false,
     event = "BufWinEnter",
     config = function()
         require("tmux").setup({})
